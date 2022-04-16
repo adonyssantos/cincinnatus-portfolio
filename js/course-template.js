cad8446cb73dc1ebdea3b3afe970f9d9e0f96017
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-04-16 10:13:23 -0400

    Add course templete styles

--- js/course-template.js
+++ js/course-template.js
@@ -1,10 +1,12 @@
-const courseTemplate = ({ title, image, description }) => `
+const courseTemplate = ({ title, image, description, duration }) => `
   <article class="box">
     <h2>${title}</h2>
-    <a href="${image.url}" target="_blank" title="Click to see full image">
+    <a href="${image.url}" title="Click to see full image">
       <img src="${image.url}" alt="${image.alt || title}" />
     </a>
-    <p>${description}</p>
+    <p><span>Description:</span> ${description}</p>
+    <p><span>Duration:</span> ${duration}</p>
+    <a href="${image.url}" class="button">View full image</a>
   </article>
 `;
 
